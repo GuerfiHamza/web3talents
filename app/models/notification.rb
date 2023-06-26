0ac80bc7804f84e7e4f74e9c440ed369bb068f95
Guerfi Hamza <hmzaa600@gmail.com>
2023-06-26 18:26:48 +0100

    done global variable

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
+
+  scope :unread, -> { where(read_at: nil).where(recipient_id: current_user.id) }
 end
