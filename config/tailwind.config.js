6a09e9e8b97a76f27c8bc134f441beca3b6056a5
Guerfi Hamza <hmzaa600@gmail.com>
2023-07-01 00:08:58 +0100

    added the template

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: [
     './public/*.html',
@@ -7,23 +8,98 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
-  theme: {
-    extend: {
-      colors: {
-        'background': '#130226',
-        'background-light': '#2c0854',
-        'primary': '#4A26AD',
-        'primary-light': '#6936F5',
-        'secondary': '#1BE4F2',
-        'secondary-light': '#D1FBFF',
-      },
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+  darkMode: 'class',
+    important: true,
+    theme: {
+        screens: {
+            xs: "540px",
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '12px',
+                sm: '1rem',
+                lg: '45px',
+                xl: '5rem',
+                '2xl': '13rem',
+            },
+
+        },
+
+        fontFamily: {
+            'plusjakarta': ['"Plus Jakarta Sans", sans-serif'],
+        },
+
+
+        extend: {
+            screens: {
+                lg_992: '992px',
+            },
+            colors: {
+                'dark': '#3c4858',
+                'black': '#161c2d',
+                'dark-footer': '#161c28',
+            },
+
+            boxShadow: {
+                sm: '0 2px 4px 0 rgb(60 72 88 / 0.15)',
+                DEFAULT: '0 0 3px rgb(60 72 88 / 0.15)',
+                md: '0 5px 13px rgb(60 72 88 / 0.20)',
+                lg: '0 10px 25px -3px rgb(60 72 88 / 0.15)',
+                xl: '0 20px 25px -5px rgb(60 72 88 / 0.1), 0 8px 10px -6px rgb(60 72 88 / 0.1)',
+                '2xl': '0 25px 50px -12px rgb(60 72 88 / 0.25)',
+                inner: 'inset 0 2px 4px 0 rgb(60 72 88 / 0.05)',
+                testi: '2px 2px 2px -1px rgb(60 72 88 / 0.15)',
+            },
+
+            fontSize: {
+                base: ['15px', '28px'],
+                lg: ['17px', '28px'],
+            },
+
+            spacing: {
+                0.75: '0.1875rem',
+                3.25: '0.8125rem'
+            },
+
+            height: ({
+                theme
+            }) => ({
+                '10.5': '2.625rem',
+                '85': '21.25rem',
+            }),
+            width: ({
+                theme
+            }) => ({
+                '10.5': '2.625rem',
+            }),
+
+            maxWidth: ({
+                theme,
+                breakpoints
+            }) => ({
+                '1200': '71.25rem',
+                '992': '60rem',
+                '768': '45rem',
+            }),
+
+            zIndex: {
+                1: '1',
+                2: '2',
+                3: '3',
+                999: '999',
+            },
+        },
     },
-  },
   plugins: [
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms')({
+      strategy: 'class', // only generate classes
+    }),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
-const path    = require("path")
+
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractCssChunksPlugin = require('extract-css-chunks-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require("webpack")
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'development',
   entry: {
     application: "./app/javascript/application.js"
   },
@@ -12,9 +17,38 @@ module.exports = {
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          ExtractCssChunksPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
     })
-  ]
-}
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin(),
+      new OptimizeCSSAssetsPlugin(),
+    ],
+  },
+};
